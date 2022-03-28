457b4983f709ed55fc8805401215cc0ab0714197
MrQuackDuck <61251075+MrQuackDuck@users.noreply.github.com>
2022-03-28 17:55:11 +0300

    New classes added

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,68 +15,89 @@ namespace CSharpConstructions
     {
         static void Main()
         {
-            /*var HowManyCores = new HowManyCores();
-            HowManyCores.Start();*/
+            var HowManyCores = new HowManyCores();
+            /*HowManyCores.Start();*/
 
-            /*var LearningEnums = new Enums();
-            LearningEnums.Start();*/
+            var LearningEnums = new Enums();
+            /*LearningEnums.Start();*/
 
-            /*var LearningTwoDimensionalArrays = new TwoDimensionalArray();
-            LearningTwoDimensionalArrays.Start();*/
+            var LearningTwoDimensionalArrays = new TwoDimensionalArray();
+            /*LearningTwoDimensionalArrays.Start();*/
 
-            /*var LearningStructs = new Structs();
-            LearningStructs.Start();*/
+            var LearningStructs = new Structs();
+            /*LearningStructs.Start();*/
 
-            /*var LearningConstructors = new Constructors();
-            LearningConstructors.Start();*/
+            var LearningConstructors = new Constructors();
+            /*LearningConstructors.Start();*/
 
-            /*var LearningTuples = new Tuples();
-            LearningTuples.Start();*/
+            var LearningTuples = new Tuples();
+            /*LearningTuples.Start();*/
 
-            /*var LearningClasses = new Classes();
-            LearningClasses.Start();*/
+            var LearningClasses = new Classes();
+            /*LearningClasses.Start();*/
 
-            /*var LearningStatic = new StaticData();
-            LearningStatic.Start();*/
+            var LearningStatic = new StaticData();
+            /*LearningStatic.Start();*/
 
-            /*var LearningProperties = new Properties();
-            LearningProperties.Start();*/
+            var LearningProperties = new Properties();
+            /*LearningProperties.Start();*/
 
-            /*var LearningConstants = new Constants();
-            LearningConstants.Start();*/
+            var LearningConstants = new Constants();
+            /*LearningConstants.Start();*/
 
-            /*var LearningPartialClasses = new PartialClass();
-            LearningPartialClasses.Start();*/
+            var LearningPartialClasses = new PartialClass();
+            /*LearningPartialClasses.Start();*/
 
-            /*var LearningInheritance = new Inheritance();
-            LearningInheritance.Start();*/
+            var LearningInheritance = new Inheritance();
+            /*LearningInheritance.Start();*/
 
-            /*var LearningBaseKeyword = new BaseKeyword();
-            LearningBaseKeyword.Start();*/
+            var LearningBaseKeyword = new BaseKeyword();
+            /*LearningBaseKeyword.Start();*/
 
-            /*var Factorial = new Factorial();
-            Factorial.Start();*/
+            var Factorial = new Factorial();
+            /*Factorial.Start();*/
 
-            /*var LearningNestedClesses = new NestedClasses();
-            LearningNestedClesses.Start();*/
+            var LearningNestedClesses = new NestedClasses();
+            /*LearningNestedClesses.Start();*/
 
-            /*var LearningPolymorphism = new Polymorphism();
-            LearningPolymorphism.Start();*/
+            var LearningPolymorphism = new Polymorphism();
+            /*LearningPolymorphism.Start();*/
 
-            /*var LearningIsAsKeywords = new IsAsKeywords();
-            LearningIsAsKeywords.Start*/
+            var LearningIsAsKeywords = new IsAsKeywords();
+            /*LearningIsAsKeywords.Start();*/
 
-            /*var LearningSwitchCase = new SwitchCase();
-            LearningSwitchCase.Start();*/
+            var LearningSwitchCase = new SwitchCase();
+            /*LearningSwitchCase.Start();*/
 
-            /*var LearningExceptions = new Exceptions();
-            LearningExceptions.Start(); // This code will throw exception */
+            var LearningExceptions = new Exceptions();
+            /*LearningExceptions.Start();*/ // This code will throw exception 
 
-            /*var LearningInterfaces = new Interfaces();
-            LearningInterfaces.Start();*/
+            var LearningInterfaces = new Interfaces();
+            /*LearningInterfaces.Start();*/
 
             var LearningCollections = new Collections();
-            LearningCollections.Start();
+            /*LearningCollections.Start();*/
+
+            var LearningDelegates = new Delegates();
+            /*LearningDelegates.Start();*/
+
+            var LearningGenericDelegates = new GenericDelegates();
+            /*LearningGenericDelegates.Start();*/
+
+            var LearningEvents = new Events();
+            /*LearningEvents.Start();*/
+
+            var LearningLambdaExpressions = new LambdaExpressions();
+            /*LearningLambdaExpressions.Start();*/
+
+            var LearningIndexers = new Indexers();
+            /*LearningIndexers.Start();*/
+
+            var LearningGenericIndexers = new GenericIndexers();
+            /*LearningGenericIndexers.Start();*/
+
+            var LearningOperatorKeyword = new Operators();
+            LearningOperatorKeyword.Start();
         }
     }
 }
